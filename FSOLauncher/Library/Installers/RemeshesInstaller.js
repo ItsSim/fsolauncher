56d888025e45bdf12787a635db4badeadf245248
ItsSim <dbhdane@outlook.com>
2018-09-16 17:23:44 +0200

    1.4.2
    Added TSO archive.org FilePlanet Installer

--- FSOLauncher/Library/Installers/RemeshesInstaller.js
+++ FSOLauncher/Library/Installers/RemeshesInstaller.js
@@ -1,155 +1,153 @@
-const Modal = require('../Modal')
+const Modal = require("../Modal");
 
 class RemeshesInstaller {
-    constructor(path, FSOLauncher) {
-        this.FSOLauncher = FSOLauncher
-        this.id = Math.floor(Date.now() / 1000)
-        this.path = path
-        this.haltProgress = false
+  constructor(path, FSOLauncher) {
+    this.FSOLauncher = FSOLauncher;
+    this.id = Math.floor(Date.now() / 1000);
+    this.path = path;
+    this.haltProgress = false;
 
-        this.dl = new(require('../Download'))()
-        this.dl.add({
-            alias: 'remeshes.zip',
-            origin: FSOLauncher.remeshInfo.location,
-            destination: 'temp/'
-        })
-    }
+    this.dl = new (require("../Download"))();
+    this.dl.add({
+      alias: "remeshes.zip",
+      origin: FSOLauncher.remeshInfo.location,
+      destination: "temp/",
+    });
+  }
 
-    createProgressItem(Message, Percentage) {
-        this.FSOLauncher.View.addProgressItem(
-            "FSOProgressItem" + this.id,
-            "Remesh Pack Download",
-            this.FSOLauncher.remeshInfo.location,
-            Message,
-            Percentage
-        )
-    }
+  createProgressItem(Message, Percentage) {
+    this.FSOLauncher.View.addProgressItem(
+      "FSOProgressItem" + this.id,
+      "Remesh Pack Download",
+      "Downloading from " + this.FSOLauncher.remeshInfo.location,
+      Message,
+      Percentage
+    );
+  }
 
-    install() {
-        return this.step1()
-            .then(() => this.step2())
-            .then(() => this.step3())
-            .then(() => this.end())
-            .catch((ErrorMessage) => this.error(ErrorMessage));
-    }
+  install() {
+    return this.step1()
+      .then(() => this.step2())
+      .then(() => this.step3())
+      .then(() => this.end())
+      .catch(ErrorMessage => this.error(ErrorMessage));
+  }
 
-    step1() {
-        return this.download()
-    }
+  step1() {
+    return this.download();
+  }
 
-    step2() {
-        return this.setupDir(this.path)
-    }
+  step2() {
+    return this.setupDir(this.path);
+  }
 
-    step3() {
-        return this.extract()
-    }
+  step3() {
+    return this.extract();
+  }
 
-    error(ErrorMessage) {
-        this.haltProgress = true;
-        this.createProgressItem(
-            global.locale.FSO_FAILED_INSTALLATION,
+  error(ErrorMessage) {
+    this.haltProgress = true;
+    this.createProgressItem(global.locale.FSO_FAILED_INSTALLATION, 100);
+    this.FSOLauncher.removeActiveTask("RMS");
+    Modal.showFailedInstall("Remesh Package", ErrorMessage);
+    return Promise.reject(ErrorMessage);
+  }
+
+  end() {
+    this.createProgressItem(global.locale.INSTALLATION_FINISHED, 100);
+    this.FSOLauncher.updateInstalledPrograms();
+    this.FSOLauncher.removeActiveTask("RMS");
+    Modal.showInstalled("Remesh Package");
+  }
+
+  download() {
+    return new Promise((resolve, reject) => {
+      this.dl.start();
+      this.dl.on("end", stats => {
+        if (stats) {
+          this.cleanup();
+          return reject(global.locale.FSO_NETWORK_ERROR);
+        }
+        resolve();
+      });
+      this.updateDownloadProgress();
+    });
+  }
+
+  setupDir(dir) {
+    return new Promise((resolve, reject) => {
+      require("mkdirp")(dir, resolve);
+    });
+  }
+
+  updateDownloadProgress() {
+    setTimeout(() => {
+      let p = this.dl.getProgress();
+
+      if (p.percentage < 100) {
+        if (!this.haltProgress) {
+          this.createProgressItem(
+            global.locale.DL_CLIENT_FILES +
+              " " +
+              p.mbDownloaded +
+              " MB " +
+              global.locale.X_OUT_OF_X +
+              " " +
+              p.mbTotal +
+              " MB (" +
+              p.percentage +
+              "%)",
+            p.percentage
+          );
+        }
+
+        return this.updateDownloadProgress();
+      }
+    }, 1000);
+  }
+
+  extract() {
+    const unzipStream = require("unzip2").Extract({
+      path: this.path,
+    });
+
+    this.createProgressItem(global.locale.EXTRACTING_CLIENT_FILES, 100);
+
+    return new Promise((resolve, reject) => {
+      require("fs")
+        .createReadStream("temp/remeshes.zip")
+        .pipe(unzipStream)
+        .on("entry", entry => {
+          this.createProgressItem(
+            global.locale.EXTRACTING_CLIENT_FILES + " " + entry.path,
             100
-        );
-        this.FSOLauncher.removeActiveTask('RMS');
-        Modal.showFailedInstall('Remesh Package', ErrorMessage);
-        return Promise.reject(ErrorMessage);
-    }
-
-    end() {
-        this.createProgressItem(
-            global.locale.INSTALLATION_FINISHED,
-            100
-        );
-        this.FSOLauncher.updateInstalledPrograms();
-        this.FSOLauncher.removeActiveTask('RMS');
-        Modal.showInstalled('Remesh Package');
-    }
-
-    download() {
-        return new Promise((resolve, reject) => {
-            this.dl.start();
-            this.dl.on('end', stats => {
-                if (stats) {
-                    this.cleanup();
-                    return reject(
-                        global.locale.FSO_NETWORK_ERROR
-                    )
-                }
-                resolve();
-            });
-            this.updateDownloadProgress()
-        })
-    }
-
-    setupDir(dir) {
-        return new Promise((resolve, reject) => {
-            require('mkdirp')(dir, resolve);
-        });
-    }
-
-    updateDownloadProgress() {
-        setTimeout(() => {
-            let p = this.dl.getProgress();
-
-            if (p.percentage < 100) {
-                if (!this.haltProgress) {
-                    this.createProgressItem(
-                        global.locale.DL_CLIENT_FILES + ' ' +
-                        p.mbDownloaded + " MB " + global.locale.X_OUT_OF_X + " " + p.mbTotal + " MB (" + p.percentage + "%)",
-                        p.percentage
-                    );
-                }
-
-                return this.updateDownloadProgress()
-            }
-        }, 1000);
-    }
-
-    extract() {
-        const unzipStream = require('unzip2').Extract({
-            path: this.path
+          );
         });
 
-        this.createProgressItem(
-            global.locale.EXTRACTING_CLIENT_FILES,
-            100
-        );
+      unzipStream.on("error", err => {
+        this.cleanup();
+        return reject(err);
+      });
 
-        return new Promise((resolve, reject) => {
-            require('fs').createReadStream('temp/remeshes.zip')
-                .pipe(unzipStream)
-                .on('entry', entry => {
-                    this.createProgressItem(
-                        global.locale.EXTRACTING_CLIENT_FILES + ' ' + entry.path,
-                        100
-                    );
-                });
+      unzipStream.on("close", err => {
+        this.cleanup();
+        return resolve();
+      });
+    });
+  }
 
-            unzipStream.on('error', err => {
-                this.cleanup();
-                return reject(err)
-            });
+  cleanup() {
+    const fs = require("fs");
+    fs.stat("temp/remeshes.zip", function(err, stats) {
+      if (err) {
+        return;
+      }
 
-            unzipStream.on('close', err => {
-                this.cleanup();
-                return resolve()
-            })
-        })
-    }
-
-    cleanup() {
-        const fs = require('fs');
-        fs.stat('temp/remeshes.zip', function (err, stats) {
-            if (err) {
-                return;
-            }
-
-            fs.unlink('temp/remeshes.zip', function (err) {
-                if (err) return console.log(err);
-            });
-        });
-    }   
+      fs.unlink("temp/remeshes.zip", function(err) {
+        if (err) return console.log(err);
+      });
+    });
+  }
 }
 
-module.exports = RemeshesInstaller
+module.exports = RemeshesInstaller;
--- main.js
+++ main.js
@@ -1,170 +1,171 @@
-const {
-	app,
-	dialog,
-	BrowserWindow,
-	shell,
-	Tray,
-	Menu
-} = require('electron')
+const { app, dialog, BrowserWindow, shell, Tray, Menu } = require("electron");
 
-const oslocale = require('os-locale')
-const fs = require('fs')
-const ini = require('ini')
-const UIText = require('./FSOLauncher_UI/UIText.json')
+const oslocale = require("os-locale");
+const fs = require("fs");
+const ini = require("ini");
+const UIText = require("./FSOLauncher_UI/UIText.json");
 
 //require('electron-reload')(__dirname);
 
-let FSOLauncher = require('./FSOLauncher/FSOLauncher')
+let FSOLauncher = require("./FSOLauncher/FSOLauncher");
 
 // Launcher title
-process.title = 'FreeSO Launcher Beta'
+process.title = "FreeSO Launcher Beta";
 
 // Launcher version
-global.version = '1.4.1'
+global.version = "1.4.2";
 
-let Window = null
-let tray = null
-let options = {}
+let Window = null;
+let tray = null;
+let options = {};
 
-global.willQuit = false
+global.willQuit = false;
 
-let code = oslocale.sync().substring(0, 2)
+let code = oslocale.sync().substring(0, 2);
 
-global.locale = UIText.hasOwnProperty(code) ?
-	UIText[code] : UIText['en']
+global.locale = UIText.hasOwnProperty(code) ? UIText[code] : UIText["en"];
 
-global.locale.LVERSION = global.version
+global.locale.LVERSION = global.version;
 
-require('electron-pug')({
-	pretty: true
-}, global.locale)
+require("electron-pug")(
+  {
+    pretty: true,
+  },
+  global.locale
+);
 
-PreCheckConfiguration()
+PreCheckConfiguration();
 
-let conf = ini.parse(
-	require('fs').readFileSync('FSOLauncher.ini', 'utf-8')
-)
+let conf = ini.parse(require("fs").readFileSync("FSOLauncher.ini", "utf-8"));
 
 /**
  * Check if launcher configuration file exists.
  * If it doesn't, create it with the default settings.
  */
 function PreCheckConfiguration() {
-	try {
-		let stats = fs.statSync('FSOLauncher.ini')
-	} catch (e) {
-		let defaultConfiguration = {
-			Launcher: {
-				Theme: 'turbo',
-				DesktopNotifications: '1',
-				Persistence: '1'
-			},
-			Game: {
-				GraphicsMode: 'ogl',
-				Language: 'en',
-				TTS: '0'
-			}
-		}
+  try {
+    let stats = fs.statSync("FSOLauncher.ini");
+  } catch (e) {
+    let defaultConfiguration = {
+      Launcher: {
+        Theme: "turbo",
+        DesktopNotifications: "1",
+        Persistence: "1",
+      },
+      Game: {
+        GraphicsMode: "ogl",
+        Language: "en",
+        TTS: "0",
+      },
+    };
 
-		fs.writeFileSync('FSOLauncher.ini', ini.stringify(defaultConfiguration), 'utf-8')
-	}
+    fs.writeFileSync(
+      "FSOLauncher.ini",
+      ini.stringify(defaultConfiguration),
+      "utf-8"
+    );
+  }
 }
 /**
  * Creates a new Electron BrowserWindow.
  */
 function CreateWindow() {
-	tray = new Tray('beta.ico')
+  tray = new Tray("beta.ico");
 
-	let width = 1100
-	let height = 675
+  let width = 1100;
+  let height = 675;
 
-	options['minWidth'] = width
-	options['minHeight'] = height
-	options['maxWidth'] = width
-	options['maxHeight'] = height
-	options['center'] = true
-	options['maximizable'] = false
-	options['width'] = width
-	options['height'] = height
-	options['show'] = false
-	options['resizable'] = false
-	options['title'] = 'FreeSO Launcher ' + global.version
-	options['icon'] = 'beta.ico'
+  options["minWidth"] = width;
+  options["minHeight"] = height;
+  options["maxWidth"] = width;
+  options["maxHeight"] = height;
+  options["center"] = true;
+  options["maximizable"] = false;
+  options["width"] = width;
+  options["height"] = height;
+  options["show"] = false;
+  options["resizable"] = false;
+  options["title"] = "FreeSO Launcher " + global.version;
+  options["icon"] = "beta.ico";
 
-	Window = new BrowserWindow(options)
+  Window = new BrowserWindow(options);
 
-	Window.setMenu(null)
-	//Window.openDevTools({mode:'detach'})
-	Window.loadURL('file://' + __dirname + '/FSOLauncher_UI/FSOLauncher.pug')
+  Window.setMenu(null);
+  //Window.openDevTools({mode:'detach'})
+  Window.loadURL("file://" + __dirname + "/FSOLauncher_UI/FSOLauncher.pug");
 
-	FSOLauncher = new FSOLauncher(Window, conf)
+  FSOLauncher = new FSOLauncher(Window, conf);
 
-	const trayTemplate = [{
-		label: global.locale.TRAY_LABEL_1,
-		click: () => {
-			FSOLauncher.onPlay()
-		}
-	}, {
-		type: "separator"
-	}, {
-		label: global.locale.TRAY_LABEL_2,
-		click: () => {
-			const notify = require('electron-notify')
-			global.willQuit = true
-			notify.closeAll()
-			Window.close()
-		}
-	}];
+  const trayTemplate = [
+    {
+      label: global.locale.TRAY_LABEL_1,
+      click: () => {
+        FSOLauncher.onPlay();
+      },
+    },
+    {
+      type: "separator",
+    },
+    {
+      label: global.locale.TRAY_LABEL_2,
+      click: () => {
+        const notify = require("electron-notify");
+        global.willQuit = true;
+        notify.closeAll();
+        Window.close();
+      },
+    },
+  ];
 
-	const ContextMenu = Menu.buildFromTemplate(trayTemplate)
+  const ContextMenu = Menu.buildFromTemplate(trayTemplate);
 
-	tray.setToolTip('FreeSO Launcher ' + global.version)
-	tray.setContextMenu(ContextMenu)
+  tray.setToolTip("FreeSO Launcher " + global.version);
+  tray.setContextMenu(ContextMenu);
 
-	tray.on('click', () => {
-		Window.isVisible() ? Window.hide() : Window.show()
-	})
+  tray.on("click", () => {
+    Window.isVisible() ? Window.hide() : Window.show();
+  });
 
-	Window.on('closed', () => {
-		Window = null
-	})
+  Window.on("closed", () => {
+    Window = null;
+  });
 
-	Window.once('ready-to-show', () => {
-		Window.show()
-	})
+  Window.once("ready-to-show", () => {
+    Window.show();
+  });
 
-	Window.on('close', e => {
-		if (!global.willQuit && FSOLauncher.conf.Launcher.Persistence === '1') {
-			e.preventDefault()
-			Window.minimize()
-		} else {
-			const notify = require('electron-notify')
-			notify.closeAll()
-		}
-	})
+  Window.on("close", e => {
+    if (!global.willQuit && FSOLauncher.conf.Launcher.Persistence === "1") {
+      e.preventDefault();
+      Window.minimize();
+    } else {
+      const notify = require("electron-notify");
+      notify.closeAll();
+    }
+  });
 
-	Window.webContents.on('new-window', (e, url) => {
-		e.preventDefault()
-		shell.openExternal(url)
-	})
+  Window.webContents.on("new-window", (e, url) => {
+    e.preventDefault();
+    shell.openExternal(url);
+  });
 }
 
-app.on('ready', CreateWindow)
+app.on("ready", CreateWindow);
 
-app.on('before-quit', function () {
-	tray.destroy()
-})
+app.on("before-quit", function() {
+  tray.destroy();
+});
 
-app.on('window-all-closed', () => {
-	app.quit()
-})
+app.on("window-all-closed", () => {
+  app.quit();
+});
 
-app.on('activate', () => {
-	null === Window && CreateWindow()
-})
+app.on("activate", () => {
+  null === Window && CreateWindow();
+});
 
 let shouldQuit = app.makeSingleInstance((a, b) => {
-	Window && (Window.show(), Window.focus())
-})
+  Window && (Window.show(), Window.focus());
+});
 
-shouldQuit && app.quit()
+shouldQuit && app.quit();
