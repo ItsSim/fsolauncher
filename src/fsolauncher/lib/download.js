cb176a17db51b6229f7e4c41e8649418ee1ad3ba
ItsSim <dbhdane@outlook.com>
2023-07-18 17:17:59 +0200

    User-Agent in axios call

--- src/fsolauncher/lib/download.js
+++ src/fsolauncher/lib/download.js
@@ -66,7 +66,10 @@ module.exports = function( { from, to, immediate = false } ) {
       method: 'get',
       url: from,
       responseType: 'stream',
-      headers: { 'Pragma': 'no-cache' },
+      headers: {
+        'Pragma': 'no-cache',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+      },
       cancelToken: _cancelSource.token,
     } )
       .then( ( response ) => {
