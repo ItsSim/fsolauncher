13db71914830102953cffb55af47e324dd38e203
ItsSim <dbhdane@outlook.com>
2023-07-18 20:04:18 +0200

    Update download.js

--- src/fsolauncher/lib/download.js
+++ src/fsolauncher/lib/download.js
@@ -67,8 +67,7 @@ module.exports = function( { from, to, immediate = false } ) {
       url: from,
       responseType: 'stream',
       headers: {
-        'Pragma': 'no-cache',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        'Pragma': 'no-cache'
       },
       cancelToken: _cancelSource.token,
     } )
