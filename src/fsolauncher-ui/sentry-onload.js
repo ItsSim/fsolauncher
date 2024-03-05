61372f659e8d29a1311896e6c6c85aea88b4f603
ItsSim <dbhdane@outlook.com>
2024-03-05 05:35:47 +0100

    chore: remove unused logging code

--- src/fsolauncher-ui/sentry-onload.js
+++ src/fsolauncher-ui/sentry-onload.js
@@ -1,9 +1,6 @@
 ( () => {
   window.Sentry.onLoad( function() {
     window.Sentry.init( {
-      beforeSend( event ) {
-        return sanitizeEvent( event );
-      },
       integrations: [
         window.Sentry.replayIntegration( {
           maskAllText: false,
@@ -13,32 +10,4 @@
       ]
     } );
   } );
-
-  function sanitizeEvent( event ) {
-    event = sanitizeExceptions( event );
-
-    return event;
-  }
-
-  function sanitizeExceptions( event ) {
-    if ( event.exceptions && event.exceptions.values ) {
-      event.exceptions.values.forEach( ( exception ) => {
-        if ( exception.stacktrace && exception.stacktrace.frames ) {
-          exception.stacktrace.frames.forEach( ( frame ) => {
-            frame.filename = obfuscatePath( frame.filename ); // Obfuscate local file paths
-          } );
-        }
-      } );
-    }
-    return event;
-  }
-
-  function obfuscatePath( filePath ) {
-    if ( typeof filePath !== 'string' ) {
-      return filePath;
-    }
-    // Replace user directory with a placeholder
-    const userDirectory = process.env.HOME || process.env.USERPROFILE;
-    return filePath.replace( userDirectory, '[USER_DIR]' );
-  }
 } )();
