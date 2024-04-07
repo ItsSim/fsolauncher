d841f9335f1728687a724561eada8b371b51c51e
ItsSim <dbhdane@outlook.com>
2024-04-07 02:55:30 +0200

    ci: fix sudo prefix

--- app/tests/e2e/stubs/sudo-prompt.js
+++ app/tests/e2e/stubs/sudo-prompt.js
@@ -1,6 +1,10 @@
 module.exports = {
   exec: ( command, options, callback ) => {
     console.info( 'stub sudo-prompt called' );
-    require( 'child_process' ).exec( command, options, callback );
+    const sudoCommand = command
+      .split( '&&' )
+      .map( cmd => 'sudo ' + cmd.trim() )
+      .join( ' && ' );
+    require( 'child_process' ).exec( sudoCommand, options, callback );
   }
 };
